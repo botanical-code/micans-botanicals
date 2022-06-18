9cd7616d89261eba499cfa70ac01de7639de8a78
Cody Potter <me@codypotter.com>
2022-06-17 17:02:41 -0700

    base navigation bar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationBarComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
